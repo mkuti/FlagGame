1612365c1d32c6d29ecead5a64f34cdad0816b9a
mkuti <pionste@gmail.com>
2019-11-11 19:14:52 +0000

    fetched the url for flags of every country before I get only one of them displayed for each click in the quiz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
-let flag = document.querySelector("#flag");
+// setting variables for the whole game
+
+let flag = document.getElementById("#flag");
 let answers = Array.from(document.getElementsByClassName("answer-item"));
 
+let flagNow = {};
+let acceptingAnswers= true;
+let availableQuestions = [];
+
+// creating array of 50 objects which represent each challenge
+//within each object, will fetch a flag and offer 4 choices of country
+
+
+//fetching rest country API
 const url = 'https://restcountries.eu/rest/v2/all';
 
 console.log(answers);
@@ -8,9 +19,12 @@ console.log(answers);
 function returnData (url) {
     return fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.forEach(item => {
+            console.log(item.flag)
+        })
+    })
     .catch(err => console.log(err))
 };
 
 returnData(url);
-
